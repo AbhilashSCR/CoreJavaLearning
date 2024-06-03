9e84db6934ad201fc894aa61ac80827141ec6f5a
Abhilash Sharma <Abhilash Sharma@LAPTOP-GHQDF5KT>
2024-06-03 08:20:23 +0530

    First Commit

---
+++ CoreJavaLearning/src/CentricToAll1/Lab001.java
@@ -0,0 +1,15 @@
+package CentricToAll1;
+
+public class Lab001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Java World");
+		System.out.println("Welcome to Course Learning");
+		System.out.println("Get Trained");
+		System.out.println("Get Skilled");
+		System.out.println("Get Employed");
+	}
+
+}
