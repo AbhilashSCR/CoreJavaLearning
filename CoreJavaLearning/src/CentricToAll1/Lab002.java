846354c2f40f32e61784248490efd35355182e3f
Abhilash Sharma <vemulaabhilash8433@gmail.com>
2024-06-04 08:58:59 +0530

    Commit

---
+++ CoreJavaLearning/src/CentricToAll1/Lab002.java
@@ -0,0 +1,19 @@
+package CentricToAll1;
+
+public class Lab002 {
+	
+	public static void main(String[] args)
+	{
+		//Boolean value can be either true/false
+		
+		boolean IsJavaIntresting=true;
+		boolean IsJavaBoring=false;
+		
+		System.out.println("Yeah, Java is quite intresting");
+		System.out.print("Nope, Java is not boring");
+		
+		
+		
+	}
+
+}
