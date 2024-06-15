296dae15562237a54469226fde3100383bdeb496
Abhilash Sharma <vemulaabhilash8433@gmail.com>
2024-06-15 18:01:35 +0530

    Commit

---
+++ CoreJavaLearning/src/CentricToAll1/Lab022.java
@@ -0,0 +1,30 @@
+package CentricToAll1;
+
+public class Lab022 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Concatenation thing
+		
+		//Concatenating Integers
+		 int a =10;
+		 int b=20;
+		 System.out.println(a+b);
+		 
+		 
+		 //Concatenating Strings and Integers
+		 
+		 String name="Vicky";
+		 System.out.println(name+a);
+		 System.out.println(a+name);
+		 System.out.println(name+a+b);  //It works as a concatenation
+		 System.out.println(a+b+name);  //Here integers are added
+		 System.out.println(name+(a+b)); //Here integers are added
+		  
+		 
+		 
+		 
+	}
+
+}
