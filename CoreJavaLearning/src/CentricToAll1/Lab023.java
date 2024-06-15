1900caa72746207085dd0afe664d401376ab9908
Abhilash Sharma <vemulaabhilash8433@gmail.com>
2024-06-15 18:25:31 +0530

    Commit

---
+++ CoreJavaLearning/src/CentricToAll1/Lab023.java
@@ -0,0 +1,24 @@
+package CentricToAll1;
+
+public class Lab023 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Relational operators: > < >= <= == != instance of
+		
+		int Father_age=62;
+		int Mother_age=56;
+		int Daughter_age=32;
+		int Son_age=28;
+		
+		boolean result1=Father_age>Mother_age;
+		boolean result2=Daughter_age<Son_age;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+
+	}
+
+}
