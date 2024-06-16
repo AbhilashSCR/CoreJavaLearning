bdc112ca691294c83df0ffec8d4a105db4ef713e
AbhilashSCR <vemulaabhilash03@gmail.com>
2024-06-16 19:33:21 +0530

    Commit

---
+++ CoreJavaLearning/src/CentricToAll1/Lab024.java
@@ -0,0 +1,21 @@
+package CentricToAll1;
+
+public class Lab024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Relational operators using characters
+		
+		System.out.println('A'==65);
+		System.out.println('B'==66);
+		System.out.println('H'==72);
+		System.out.println('I'==73);
+		System.out.println('L'==76);
+		System.out.println('A'==65);
+		System.out.println('S'==83);
+		System.out.println('H'==72);
+		
+	}
+
+}
