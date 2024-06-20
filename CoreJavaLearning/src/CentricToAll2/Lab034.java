793ea4293743a684c098130183f9c9c6fdc99427
AbhilashSCR <vemulaabhilash03@gmail.com>
2024-06-20 22:14:48 +0530

    Commit

---
+++ CoreJavaLearning/src/CentricToAll2/Lab034.java
@@ -0,0 +1,34 @@
+package CentricToAll2;
+
+public class Lab034 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		boolean b1=!true;
+		if(b1==true)
+		{
+			System.out.println("boolean value is true");
+		}
+		else
+		{
+			System.out.println("boolean value is false");
+		}
+		
+		
+		int number1=977;
+		int number2=452;
+		if (number1!=number2)
+		{
+			System.out.println("Given number is odd number");
+		}
+		else
+		{
+			System.out.println("Given number is even number");
+		}
+	
+		
+	}
+
+}
---
+++ CoreJavaLearning/src/CentricToAll2/Lab035.java
@@ -0,0 +1,27 @@
+package CentricToAll2;
+import java.util.Scanner;
+public class Lab035 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//if else condition using scanner class.
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter the number:");
+		int number=sc.nextInt();
+		
+		if (number%2==0 )
+		{
+		System.out.println("Entered number is even number");
+		}
+		else
+		{
+			System.out.println("Entered number is odd number");
+		}
+		
+		
+	}
+
+}
