1e9d237718e3788c47e6b027e0b6060cc5149e6e
AbhilashGUI <vemulaabhilash8433@gmail.com>
2024-08-27 10:11:23 +0530

    Commit

---
+++ CoreJavaLearning/src/CentricToAll7_Customizedexceptions_CEE/Lab179.java
@@ -0,0 +1,31 @@
+package CentricToAll7_Customizedexceptions_CEE;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Lab179 {
+	
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		
+		//int a=10/0;  //Note: This is an example of unchecked exception. Where JVM doesn't know about the error in advance.
+
+		
+	/***	try {
+			FileReader f= new FileReader(new File("c://Abhilash.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}***/
+		//Note:This is an example of checked exception.Where JVM is aware about the error in advance
+	
+		FileReader f= new FileReader(new File("c://Abhilash.txt"));
+	
+	}
+
+	//In the first piece of code, we have used try&catch block , where as in the 
+	//Second piece of code, we have used throws keyword at the method level
+	//Relates with the example pipe and a patch
+	
+	
+}
---
+++ CoreJavaLearning/src/CentricToAll7_Customizedexceptions_CEE/Lab180.java
@@ -0,0 +1,20 @@
+package CentricToAll7_Customizedexceptions_CEE;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Lab180 {
+
+	public static void main(String[] args)
+	{
+		String path="c://Abhilash.txt";
+		try {
+			FileReader f= new FileReader(new File("c://Abhilash.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+//In this case, we have had a choice of using try&catch block or throws keyword at the method level
